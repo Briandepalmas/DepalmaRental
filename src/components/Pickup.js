07454b93519e7453e7ba7ec5b1e0b8980036234f
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-30 16:53:50 -0400

    pick up component

---
+++ src/components/Pickup.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Pickup extends Component {
+    render() {
+        return (
+            <div>
+
+                <select>
+                    <option value="NY">New York</option>
+                    <option value="TX">Dallas</option>
+                </select>  
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>  
+            </div>
+        )
+    }
+}
